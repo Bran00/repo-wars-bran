fb9dbd539730ce8da509858283c81c365801e09c
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-03-31 17:41:36 -0300

    fix: vite config without global

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {alias: {"node-fetch": "axios"}},
-  define: {
-    global: {}
-  },
+  resolve: { alias: { "node-fetch": "axios" } },
 })
